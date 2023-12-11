ff00f751d3df477ca7e6099e0040f43ada86b951
swati-05 <swatisharma52000@gmail.com>
2023-12-11 16:00:40 +0530

    11/12/23

--- src/Component/Home/Section1.js
+++ src/Component/Home/Section1.js
@@ -21,10 +21,10 @@ function Section1() {
 
   return (
     <>
-      <div className='' style={{
-        backgroundImage: `url("https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww")`, height: '48rem', width: '100%', backgroundRepeat: 'repeat',
+      <div className='h-auto' style={{
+        backgroundImage: `url("https://images.pexels.com/photos/434337/pexels-photo-434337.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,  width: '100%', backgroundRepeat: 'repeat' ,
       }}>
-        <div className='bg-[#463492] opacity-90 md:p-32  lg:p-32'>
+        <div className='bg-[#463492] opacity-80 '>
           {/* header */}
           <header class="text-gray-600 body-font ">
             <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center ">
@@ -33,7 +33,7 @@ function Section1() {
                   <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                 </svg>
               </a>
-              <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center text-gray-50">
+              <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center text-gray-50 mt-12">
                 <Link to="home" spy={true} smooth={true} duration={500} offset={-50}>
                   <button className="mr-5 hover:text-gray-50">Home</button>
                 </Link>
@@ -54,16 +54,16 @@ function Section1() {
             </div>
           </header>
           {/* section contact */}
-          <section class="text-gray-600 body-font">
+          <section class="text-gray-600 body-font p-4 mt-16 pb-36">
             <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
               <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 ">
                 <img class=" object-center rounded-full w-64 float-right" alt="hero" src="https://cdn.hero.page/pfp/5faf490c-f67d-4f2a-8098-a116a018637a-close-up-of-kawaii-shoujo-girl-cute-anime-profile-pictures-for-girls-2.png" />
               </div>
-              <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                <h1 class="title-font sm:text-4xl text-6xl mb-4 font-medium text-gray-50">Hello I'm Christy Smith!
+              <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16  flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 class="title-font text-3xl sm:text-5xl mb-4 font-medium text-gray-50">Hello I'm Christy Smith!
                   <br class="hidden lg:inline-block" />
                 </h1>
-                <p class="mb-8 leading-relaxed text-gray-100 ">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                  <p class="mb-8 leading-relaxed text-gray-100 ">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
                 <div class="flex justify-center space-x-2">
                   {/* <h1 className='font-bold text-gray-50 uppercase text-lg'>LET'S CONNECT</h1>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mt-1 text-amber-400 w-6 h-6">
